711d4cd55a947d802e12d207539d715f790ec90c
Gustavo Henrique Domingues <64338037+gudomingues@users.noreply.github.com>
2025-04-24 12:15:16 -0300

    commit

--- innovation_project/urls.py
+++ innovation_project/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', RedirectView.as_view(url='/cadastros/', permanent=True)),
     path('admin/', admin.site.urls),
     path('cadastros/', include('cadastros.urls')),
     path('relatorios/', include('relatorios.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
